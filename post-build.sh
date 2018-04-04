1a1ab8363c8a2b437b07eb1f3c552ce5e3a793b6
Jan-Philipp Litza <janphilipp@litza.de>
2018-04-04 18:24:45 +0200

    buildroot: Improve init scripts

--- post-build.sh
+++ post-build.sh
@@ -3,4 +3,15 @@
 set -u
 set -e
 
-rm -f "${TARGET_DIR}/etc/init.d/S99snapclient" "${TARGET_DIR}/usr/bin/snapserver"
+rm -f \
+    "${TARGET_DIR}/etc/init.d/S99snapclient" \
+    "${TARGET_DIR}/etc/init.d/S20urandom" \
+    "${TARGET_DIR}/usr/bin/snapserver"
+
+for f in "${TARGET_DIR}/etc/init.d/"S*; do
+    if [ "${f%.sh}" != "$f" ]; then
+        # already has .sh extension
+        continue
+    fi
+    mv "$f" "${f}.sh"
+done
